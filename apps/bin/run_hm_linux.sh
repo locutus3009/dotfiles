d0bf267e497b8ec506f9b67f453cfaed645fe813
Nick Nerovny <nikolay.nerovniy@gmail.com>
2024-06-24 21:16:13 +0200

    Simplify
    Signed-off-by: Nick Nerovny <nikolay.nerovniy@gmail.com>

--- apps/bin/run_hm_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export T=virt-hyp
-export E=dev
-export VM_IMAGE=/home/locutus/dev/hm-grc-scripts/files/guest_virt.img
-export IMAGE_ROOTFS_MANIFEST=/home/locutus/dev/lotto//src/hm-lotto/lotto.manifest
-export UVMM_VM_IMAGE=/home/locutus/dev/hm-grc-scripts/hm-uvmm/tests/build/guest/guestpayload.img
-export SDK_VM_IMAGE=/home/locutus/dev/hm-grc-scripts/SDK/sysroots/x86_64-eulersdk-linux/../../sysroots/aarch64-euler-elf/usr/bin/guestpayload.img
-./scripts/run-hm.sh
