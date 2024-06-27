bdebb28862b434fee001d03abf1a7c4512943923
Nikolay Nerovny <nikolay.nerovniy@gmail.com>
2024-06-27 20:49:11 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -sS https://starship.rs/install.sh | sh
+# curl -sS https://starship.rs/install.sh | sh
 
-sudo apt install picom mpd xfonts-terminus xbindkeys rofi fonts-inter redshift blueman mpc alsa-utils pulseaudio acpi acpid acpi-call-dkms maim feh xclip xfce4-power-manager light exuberant-ctags global ibus numlockx shfmt gnome-audio
+yay -S picom mpd xfonts-terminus xbindkeys rofi fonts-inter redshift blueman mpc alsa-utils pulseaudio acpi acpid acpi-call-dkms maim feh xclip xfce4-power-manager light exuberant-ctags global ibus numlockx shfmt gnome-audio
